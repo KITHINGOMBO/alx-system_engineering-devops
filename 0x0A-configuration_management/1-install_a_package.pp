2eec15987da8ad1cea4383832c707d4340e08dc0
KITHINGOMBO <denniskithi@gmail.com>
2023-03-25 13:02:28 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # install puppet-lint -v 2.5.0
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
 }
